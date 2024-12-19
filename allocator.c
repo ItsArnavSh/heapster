6be18e79d7744971d792a3652fd540c6e4dab324
ItsArnavSh <its.arnav.sh@gmail.com>
2024-12-19 16:40:39 +0530

    Lets Start Over

--- allocator.c
+++
@@ -1,8 +0,0 @@
-#include "allocator.h"
-#include <stdio.h>
-
-void* allocate(uint32_t bytes){
-    //Okay, first we need to figure out how much memory we need to access
-    void* match = bestMatch(bytes+1);//+1 so that we can include the metadata
-    return match+1;
-}
--- allocator.h
+++
@@ -1,7 +0,0 @@
-#ifndef ALLOCATOR_H
-#define ALLOCATOR_H
-#include <stdint.h>
-#include <sys/types.h>
-#include "memStatus.h"
-void* allocate(uint32_t bytes);
-#endif
